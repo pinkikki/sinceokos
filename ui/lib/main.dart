0c940225c3635031d93d3e7c144d0c40541a9089
pinkikki <w-mizutani@pinkikki.jp>
2019-11-01 08:34:25 +0900

    diariesを表示してみた

--- ui/lib/main.dart
+++ ui/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:sinceokos_ui/port/diary.pb.dart';
 import 'package:sinceokos_ui/port/diary_service.dart';
 
 void main() => runApp(MyApp());
@@ -8,11 +8,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'diaries',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.teal,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'diaries'),
     );
   }
 }
@@ -27,12 +27,12 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  String _text = "";
+  List<DiaryResource> _diaries = List();
 
   Future<void> _get() async {
-    var response = await DiaryService.get();
+    var response = await DiaryService.list();
     setState(() {
-      _text = response.text;
+      _diaries = response.diaries;
     });
   }
 
@@ -42,23 +42,88 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_text',
-              style: Theme
-                  .of(context)
-                  .textTheme
-                  .display1,
-            ),
-          ],
-        ),
-      ),
+//      body: Center(
+//        child: Column(
+//          mainAxisAlignment: MainAxisAlignment.center,
+//          children: <Widget>[
+//            Text(
+//              'You have pushed the button this many times:',
+//            ),
+//            Text(
+//              '$_text',
+//              style: Theme
+//                  .of(context)
+//                  .textTheme
+//                  .display1,
+//            ),
+//          ],
+//        ),
+//      ),
+      body: ListView.builder(
+          itemCount: _diaries.length,
+          itemBuilder: (context, index) {
+            var diary = _diaries[index];
+            return Column(
+              children: <Widget>[
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Padding(
+                          padding:
+                              const EdgeInsets.fromLTRB(12.0, 12.0, 12.0, 6.0),
+                          child: Text(
+                            diary.no,
+                            style: TextStyle(
+                                fontSize: 22.0, fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                        Padding(
+                          padding:
+                              const EdgeInsets.fromLTRB(12.0, 6.0, 12.0, 12.0),
+                          child: Text(
+                            diary.text,
+                            style: TextStyle(fontSize: 18.0),
+                          ),
+                        ),
+                      ],
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: <Widget>[
+                          Text(
+                            "5m",
+                            style: TextStyle(color: Colors.grey),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Icon(
+                              Icons.star_border,
+                              size: 35.0,
+                              color: Colors.grey,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+                Divider(
+                  height: 2.0,
+                  color: Colors.grey,
+                )
+              ],
+            );
+//            return ListTile(
+//              leading: Icon(Icons.crop_din),
+//              title: Text(diary.no),
+//              subtitle: Text(diary.text),
+//            );
+          }),
       floatingActionButton: FloatingActionButton(
         onPressed: _get,
         tooltip: 'Increment',
--- ui/lib/port/diary_service.dart
+++ ui/lib/port/diary_service.dart
@@ -1,11 +1,17 @@
 import 'package:sinceokos_ui/port/diary.pbgrpc.dart';
+import 'package:sinceokos_ui/port/google/protobuf/empty.pb.dart';
 import 'package:sinceokos_ui/port/grpc_client.dart';
 
 class DiaryService {
-  static Future<DiaryResource> get() async{
+  static Future<DiaryResource> get() async {
     var client = DiaryClient(GrpcClientSingleton().client);
     var criteria = DiaryCriteria.create();
     criteria.no = "10001";
     return await client.get(criteria);
+  }
+
+  static Future<DiaryResources> list() async {
+    var client = DiaryClient(GrpcClientSingleton().client);
+    return await client.list(Empty.create());
   }
 }
